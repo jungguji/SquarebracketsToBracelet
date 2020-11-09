d8e9b16896ed60e7cfbb6e4ea58e331ff9de7754
junggu.ji <junggu.ji.dev@gmail.com>
2020-11-09 20:00:11 +0900

    type : type
    * 문자열 상수 properties로 이동

--- src/main/java/com/ji/junggu/bracelet/Main.java
+++ src/main/java/com/ji/junggu/bracelet/Main.java
@@ -1,6 +1,7 @@
 package com.ji.junggu.bracelet;
 
 import com.ji.junggu.bracelet.domain.convert.domain.Convert;
+import com.ji.junggu.bracelet.global.util.PropertiesUtil;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,10 +10,10 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            System.out.print("자료형(int, String, char) 입력 : ");
+            System.out.print(PropertiesUtil.getMessage("message.insert.data.type"));
             String type = br.readLine();
 
-            System.out.print("괄호 치환 할 문자열 입력 : ");
+            System.out.print(PropertiesUtil.getMessage("message.insert.content"));
             String input = br.readLine();
             int dimension = getDimension(input);
 
